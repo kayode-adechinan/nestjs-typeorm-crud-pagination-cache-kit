b4305e49d33c76f1c99d7d2753decf1df96d7e4a
Kayode Adechinan <kayode.adechinan@gmail.com>
2022-01-17 19:42:00 +0000

    (feat): adding caching both in-memory and with redis

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,41 +1,10 @@
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 import * as dotenv from 'dotenv';
+import { ConnectionOptions } from 'typeorm';
 dotenv.config();
 
-const configDevelopment: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: 'db',
-  entities: ['dist/src/**/*.entity.js'],
-  synchronize: false,
-  migrations: ['dist/src/db/migrations/*.js'],
-  cli: {
-    migrationsDir: 'src/db/migrations',
-  },
-};
-
-const configTest: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: 'dbtest',
-  entities: ['./**/*.entity.ts'],
-  synchronize: true,
-  dropSchema: true,
-};
-
-const sqliteConfig: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: 'db',
-  entities: ['dist/src/**/*.entity.js'],
-  synchronize: false,
-  migrations: ['dist/src/db/migrations/*.js'],
-  cli: {
-    migrationsDir: 'src/db/migrations',
-  },
-};
-
-const pgConfig: PostgresConnectionOptions = {
+const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'database',
+  host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -50,9 +19,4 @@ const pgConfig: PostgresConnectionOptions = {
 
 //const config = process.env.NODE_ENV === 'test' ? configTest : configDevelopment;
 
-let config: SqliteConnectionOptions | PostgresConnectionOptions = sqliteConfig;
-
-if (process.env.DB_TYPE) {
-  config = pgConfig;
-}
 export default config;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module, CacheInterceptor } from '@nestjs/common';
+
+import * as redisStore from 'cache-manager-redis-store';
+
 import { ItemModule } from './item/item.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SharedModule } from './shared/shared.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import config from '../ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), ItemModule, SharedModule],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 5, // seconds
+      max: 10, // maximum number of items in cache
+      // uncomment the three lines bellow to swith to redis based caching otherwise you will get in memory caching
+      // store: redisStore,
+      // host: 'redis', // name of the service in docker-compose file, otherwise replace it with 'localhost'
+      // port: 6379,
+    }),
+
+    TypeOrmModule.forRoot(config),
+    ItemModule,
+    SharedModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
